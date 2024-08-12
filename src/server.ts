c5a1f3279dafe234685a38c615b2b1ef26ef8888
qzda <qinzhidaoa@gmail.com>
2024-08-12 16:59:54 +0800

    feat(server): dynamic directory support

--- src/server.ts
+++ src/server.ts
@@ -2,16 +2,23 @@ import chalk from "chalk";
 import express from "express";
 import { version } from "../package.json";
 import { readdir } from "node:fs/promises";
+import path from "node:path";
 
 export function startServer() {
   const PORT = process.env.PORT || 3000;
   const app = express();
 
-  app.use(express.static("."));
+  app.use(
+    express.static(".", {
+      dotfiles: "allow",
+    })
+  );
 
   app.set("view engine", "pug");
-  app.get("/", async (req, res) => {
-    const files = await readdir(".");
+  app.get("*", async (req, res) => {
+    // console.log(req);
+    const urlPath = path.join(".", req.path);
+    const files = await readdir(urlPath);
 
     res.render("index", {
       title: "hsplus",
