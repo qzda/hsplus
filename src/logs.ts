cb1e6ced1de08166e2499ba8c374824584839148
qzda <qinzhidaoa@gmail.com>
2024-11-27 14:47:49 +0800

    feat(log): update help info

--- src/logs.ts
+++ src/logs.ts
@@ -14,18 +14,10 @@ export function logHelp() {
   console.log();
 
   console.log(prolog.bold("Options:"));
-  console.log(
-    `  ${prolog.cyan(
-      "-p, --port"
-    )}${tab}${"Port to use. Use -p 0 to look for an open port, starting at 8080. It will also read from process.env.PORT."}`
-  );
   console.log(`  ${prolog.cyan("--host")}${tab}${"Enable LAN access"}`);
-  console.log(
-    `  ${prolog.cyan("-h, --help")}${tab}${"Display this menu and exit"}`
-  );
-  console.log(
-    `  ${prolog.cyan("-v, --version")}${tab}${"Print the version and exit"}`
-  );
+  console.log(`  ${prolog.cyan("-p, --port")}${tab}${"Port to use"}`);
+  console.log(`  ${prolog.cyan("-h, --help")}${tab}${"Display help"}`);
+  console.log(`  ${prolog.cyan("-v, --version")}${tab}${"Print the version"}`);
 
   console.log();
 }
